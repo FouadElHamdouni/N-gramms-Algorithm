fc17d68234a85c76ad7208a90052381e36be4a92
Jennifer Arnold <jarnold1@macalester.edu>
2017-12-06 15:10:56 -0600

    Beginning to change code so it reads in a textfile

--- Ngrams.py
+++ Ngrams.py
@@ -1,17 +1,16 @@
 import nltk
-#nltk.download('punkt')
+# nltk.download('punkt')
 from nltk import word_tokenize
 from nltk.util import ngrams
 from collections import Counter
 
-text = "I need to write a program in NLTK that breaks a corpus (a large collection of \
-txt files) into unigrams, bigrams, trigrams, fourgrams and fivegrams.\
-I need to write a program in NLTK that breaks a corpus"
+text = set(open("1970ALB.txt", 'r'))
+
 token = nltk.word_tokenize(text)
-bigrams = ngrams(token,2)
-trigrams = ngrams(token,3)
-fourgrams = ngrams(token,4)
-fivegrams = ngrams(token,5)
+bigrams = ngrams(token, 2)
+trigrams = ngrams(token, 3)
+fourgrams = ngrams(token, 4)
+fivegrams = ngrams(token, 5)
 
 print(Counter(bigrams))
 
