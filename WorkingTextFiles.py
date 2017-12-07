f88176eff3157f76853b22176d7c547852d19ba3
Jennifer Arnold <jarnold1@macalester.edu>
2017-12-06 19:40:31 -0600

    Beginning to change code so it reads in a textfile

--- WorkingTextFiles.py
+++ WorkingTextFiles.py
@@ -1,12 +1,46 @@
 from stop_words import get_stop_words
+import string
 
-text = open('/Users/jenniferarnold/Corpus/debates1970/1970ALB.txt', 'r')
+path = '/Users/jenniferarnold/PycharmProjects/N-gramms-Algorithm/Papers/FederalistNo.84Hamilton.txt'
+titleName = path[64:]
+print(titleName)
+# Make a new file
+newFile = open(titleName, "w+")
+
+text = open(path, 'r')
 stop_words = set(get_stop_words('english'))
 
+# Open file
+file = open(path, 'r', errors='replace')
+text = file.read()
+text = text.split()
+
+
+
+# Loop over text and append to textfile document
 for line in text:
     for word in line.split():
         if word.lower() not in stop_words:
-            print(word)
+            print(word.lower())
+            newFile.write(word+" ")
+
+
+
+
+# import csv
+#
+# with open('/Users/jenniferarnold/Downloads/un-general-debates 2.csv', 'r') as csvfile:
+#     reader = csv.reader(csvfile)
+#     for row in reader:
+#         # create file with some filename in pathWhereTheseThingsGo
+#         titleName = row[1:3]
+#         titleString = ''.join(titleName)
+#
+#         # print(titleString)
+#         # print(type(row[3]))
+#         f = open(titleString, "w+")
+#
+#         f.write(row[3])
 
 
 
