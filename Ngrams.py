eb394378d37363c70345b46db20ad2fd9ac8c122
Jennifer Arnold <jarnold1@macalester.edu>
2017-12-06 19:37:56 -0600

    Beginning to change code so it reads in a textfile

--- Ngrams.py
+++ Ngrams.py
@@ -1,28 +1,30 @@
 import nltk
-# nltk.download('punkt')
+nltk.download('punkt')
 from nltk import word_tokenize
 from nltk.util import ngrams
 from collections import Counter
 
-text = set(open("1970ALB.txt", 'r'))
+file = open("/Users/jenniferarnold/PycharmProjects/N-gramms-Algorithm/Papers/FederalistNo.84Hamilton.txt", 'r', errors='replace')
+text = file.read()
+text = text.split()
 
-token = nltk.word_tokenize(text)
-bigrams = ngrams(token, 2)
-trigrams = ngrams(token, 3)
-fourgrams = ngrams(token, 4)
-fivegrams = ngrams(token, 5)
+# # print(text)
+# token = nltk.word_tokenize(text)
+# bigrams = ngrams(token, 2)
+# trigrams = ngrams(token, 3)
+# fourgrams = ngrams(token, 4)
+# fivegrams = ngrams(token, 5)
+#
+# print(Counter(bigrams))
 
-print(Counter(bigrams))
 
 
-# listgram = ["all", "is", "good", "between", "us"]
-#
-# def find_ngrams(listofGrams):
-#     bigram = []
-#     for i in range(len(listofGrams)-1):
-#         bigram.append((listofGrams[i], listofGrams[i+1]))
-#     return bigram
-#
-#
-#
-# print(find_ngrams(listgram))
+def find_ngrams(text):
+    bigram = []
+    for i in range(len(text)-1):
+        bigram.append((text[i], text[i+1]))
+    return bigram
+
+
+
+print(Counter(find_ngrams(text)))
