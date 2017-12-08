2f9a870a1aa0f2d5afe2acc8886c3c5cbd291127
Jennifer Arnold <jarnold1@macalester.edu>
2017-12-08 14:35:49 -0600

    Added in cosine similarity method

--- Ngrams.py
+++ Ngrams.py
@@ -1,14 +1,17 @@
 import nltk
-nltk.download('punkt')
+# nltk.download('punkt')
 from nltk import word_tokenize
 from nltk.util import ngrams
 from collections import Counter
+import numpy as np
+import numpy.linalg
 
-file = open("/Users/jenniferarnold/PycharmProjects/N-gramms-Algorithm/Papers/FederalistNo.84Hamilton.txt", 'r', errors='replace')
+path = "/Users/jenniferarnold/PycharmProjects/N-gramms-Algorithm/Papers_sanitized/FederalistNo_2_san.txt"
+file = open(path, 'r', errors='replace')
 text = file.read()
 text = text.split()
 
-# # print(text)
+# print(text)
 # token = nltk.word_tokenize(text)
 # bigrams = ngrams(token, 2)
 # trigrams = ngrams(token, 3)
@@ -26,5 +29,21 @@ def find_ngrams(text):
     return bigram
 
 
+def cosine_similarity(a, b):
+    dot_product = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    return dot_product / (norm_a*norm_b)
 
-print(Counter(find_ngrams(text)))
+# Example
+listOfNum = [1, 1, 1, 1, 0, 0, 0, 0, 0]
+sentence_m = np.array(listOfNum)
+sentence_h = np.array([0, 3, 1, 1, 1, 2, 0, 0, 0])
+sentence_w = np.array([0, 0, 4, 1, 0, 0, 1, 4, 1])
+
+first = cosine_similarity(sentence_h, sentence_w)
+print(first)
+
+# print(Counter(find_ngrams(text)))
+
+
